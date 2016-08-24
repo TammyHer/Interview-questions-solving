96ce3b594960457819b0ccfcc96c7bb20cb4fe5f
Tammy <misstamar@gmail.com>
2016-08-23 22:21:40 -0700

    compiled

--- problems/UCreditCard.rb
+++ problems/UCreditCard.rb
@@ -1,62 +1,62 @@
-https://www.codewars.com/kata/validate-credit-card-number/train/ruby
-
-In this 
-
-Kata, you will implement The Luhn Algorithm, which is used to help validate credit card numbers.
-
-Given a positive integer of up to 16 digits, return true if it is a valid credit card number, and false if it is not.
-
-Here is the algorithm:
-
-If there are an even number of digits, double every other digit starting with the first, and if there are an odd number of digits, double every other digit starting with the second. Another way to think about it is, from the right to left, double every other digit starting with the second to last digit.
-
-1714 => [1*, 7, 1*, 4] => [2, 7, 2, 4]
-
-12345 => [1, 2*, 3, 4*, 5] => [1, 4, 3, 8, 5]
-
-891 => [8, 9*, 1] => [8, 18, 1]
-If a resulting doubled number is greater than 9, replace it with either the sum of it's own digits, or 9 subtracted from it.
-
- [8, 18*, 1] => [8, (1+8), 1] => [8, 9, 1]
-
- (or)
-
- [8, 18*, 1] => [8, (18-9), 1] => [8, 9, 1]
-Sum all of the final digits
- [8, 9, 1] => 8+9+1 => 18
-Finally, take that sum and divide it by 10. If the remainder equals zero, the original credit card number is valid.
-
- 18 (modulus) 10 => 8.  
-
- 8 does not equal 0, so 891 is not a valid credit card number.
- 
-
+# https://www.codewars.com/kata/validate-credit-card-number/train/ruby
+#
+# In this
+#
+# Kata, you will implement The Luhn Algorithm, which is used to help validate credit card numbers.
+#
+# Given a positive integer of up to 16 digits, return true if it is a valid credit card number, and false if it is not.
+#
+# Here is the algorithm:
+#
+# If there are an even number of digits, double every other digit starting with the first, and if there are an odd number of digits, double every other digit starting with the second. Another way to think about it is, from the right to left, double every other digit starting with the second to last digit.
+#
+# 1714 => [1*, 7, 1*, 4] => [2, 7, 2, 4]
+#
+# 12345 => [1, 2*, 3, 4*, 5] => [1, 4, 3, 8, 5]
+#
+# 891 => [8, 9*, 1] => [8, 18, 1]
+# If a resulting doubled number is greater than 9, replace it with either the sum of it's own digits, or 9 subtracted from it.
+#
+#  [8, 18*, 1] => [8, (1+8), 1] => [8, 9, 1]
+#
+#  (or)
+#
+#  [8, 18*, 1] => [8, (18-9), 1] => [8, 9, 1]
+# Sum all of the final digits
+#  [8, 9, 1] => 8+9+1 => 18
+# Finally, take that sum and divide it by 10. If the remainder equals zero, the original credit card number is valid.
+#
+#  18 (modulus) 10 => 8.
+#
+#  8 does not equal 0, so 891 is not a valid credit card number.
+#
 def validate(n)
 
   n = n.to_s
   sum = 0
-  length = n.length 
-  
+  length = n.length
+
  return nil if length > 16 || n.nil? || n == 0
-    
+
   length % 2 == 0 ? start = 0 : start = 1 # start when to
-  
+
   i = start
-  while i < length-1
+  while i < length
     if (n[i].to_i * 2) > 9
-       sum += ( n[i].to_i * 2) % 10 + 1 
+       sum += ( n[i].to_i * 2) % 10 + 1
     else
       sum += n[i].to_i * 2
     end
     i += 2
   end
-  
+
   start == 0 ? start = 1 : start = 0
   i = start
-  while i < n.length-1
+  while i < n.length
     sum += n[i].to_i
     i += 2
   end
-  
-  return sum % 10 == 0 
+  return sum % 10 == 0
 end
+
+puts "yes" if validate(919)
--- problems/UncopmleterAnagram.rb
+++ problems/UncopmleterAnagram.rb
@@ -1,48 +1,42 @@
-https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/ruby
-
+# https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/ruby
+# What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+#
+# 'abba' & 'baab' == true
+#
+# 'abba' & 'bbaa' == true
+#
+# 'abba' & 'abbba' == false
+# Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+#
+# anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+#
+# anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+#
+# anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+require 'pry'
 def anagrams(word, words)
-  return [] if words.empty? 
+  return [] if words.empty?
   hashWord = Hash.new
- 
-  What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
-
-'abba' & 'baab' == true
-
-'abba' & 'bbaa' == true
-
-'abba' & 'abbba' == false
-Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
-
-anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
-
-anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
-
-anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
-  
   resultArr = []
-  result = ""
-  
-  word.split.map { |char| hashWord[char] == nil ? hashWord[char] = 1 : hashWord[char] += 1 }
-  tempHash = hashWord
-  words.each do |word|
-    for i in 0..word.length-1
-      if tempHash.include?(i)
-        if tempHash[i] != 0 
-            tempHash[i] -= 1
-          if i = word.length-1 # last char ? - need to push
-            tempHash = hashWord
-            resultArr.push(word)
+
+  word.split("").map { |char| hashWord[char] == nil ? hashWord[char] = 1 : hashWord[char] += 1 }
+  tempHash = hashWord.clone
+  words.each do |wordi|
+    tempHash = hashWord.clone
+    if wordi.length == word.length
+      for i in 0..word.length-1
+        if tempHash.include?(wordi[i]) && tempHash[wordi[i]] != 0
+          tempHash[wordi[i]] -= 1
+          if (i == word.length-1)
+            resultArr.push(wordi)
           end
         else
-          i = word.length-1
-          tempHash = hashWord
-         end
-      else
-        i = word.length-1
-        tempHash = hashWord
+          break
+        end
       end
     end
   end
-  
-  return result  
+  return resultArr
 end
+
+puts anagrams('racer', ['racar', 'racer', 'crazer', 'carer', 'caers'])
